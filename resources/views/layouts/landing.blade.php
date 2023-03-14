acb07594fdb58e2bdd555cac5cac75825b396ca6
carlosleoncode <carlosestebanleonpinilla@gmail.com>
2023-03-13 22:14:41 -0500

    feat: landing with cristian and carolina changes

--- resources/views/layouts/landing.blade.php
+++ resources/views/layouts/landing.blade.php
@@ -17,37 +17,40 @@
   @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 </head>
 <body>
-    <div id="app">
-
-      <!-- Encabezado -->
-      <header id="header" class="fixed-top">
-        <div class="container d-flex align-items-center">
-          <h1 class="logo me-auto"><a href="index.html">OMC</a></h1>
-          <nav id="navbar" class="navbar order-last order-lg-0">
-            <ul>
+  <div id="app">
+    <!-- Encabezado -->
+    <header id="header" class="fixed-top">
+      <div class="container d-flex align-items-center">
+        <h1 class="logo me-auto"><a href="index.html">OMC</a></h1>
+        <nav class="navbar navbar-expand-lg">
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="ms-auto navbar-nav">
               <li><a class="nav-link scrollto active" href="#inicio">Inicio</a></li>
               <li><a class="nav-link scrollto" href="#servicios">Servicios</a></li>
-              <li><a class="nav-link scrollto" href="#galeria">Galeria</a></li>
-              <li><a class="nav-link scrollto" href="#contacto">Contacto</a></li>
+              <li><a class="nav-link scrollto ms-2" href="#galeria">Galeria</a></li>
+              <li><a class="nav-link scrollto ms-2" href="#aboutUs">Sobre nosotros</a></li>
+              <li><a class="nav-link scrollto ms-2" href="#contacto">Contacto</a></li>
               @if (Route::has('login'))
                 <li class="nav-item">
-                    <a class="nav-link scrollto" href="{{ route('login') }}">{{ __('Login') }}</a>
+                  <a class="nav-link scrollto" href="{{ route('login') }}">{{ __('Iniciar sesión') }}</a>
                 </li>
               @endif
-
               @if (Route::has('register'))
-                  <li class="nav-item">
-                      <a class="nav-link scrollto" href="{{ route('register') }}">{{ __('Register') }}</a>
-                  </li>
+                <li class="nav-item">
+                  <a class="nav-link scrollto" href="{{ route('register') }}">{{ __('Registrarme') }}</a>
+                </li>
               @endif
-            </ul>
-            <i class="bi bi-list mobile-nav-toggle"></i>
-          </nav>
-        </div>
-      </header>
-      <main class="py-4">
-          @yield('content')
-      </main>
-    </div>
+            </div>
+          </div>
+        </nav>
+      </div>
+    </header>
+    <main class="py-4">
+      @yield('content')
+    </main>
+  </div>
 </body>
 </html>
